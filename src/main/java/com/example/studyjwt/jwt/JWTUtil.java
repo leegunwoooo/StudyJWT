4e65352a61d69a1554fafd8465a746fe8dae4415
leegunwoooo <leegunwoo0325@gmail.com>
2025-02-22 06:39:04 +0900

    secretKey final로 선언

--- src/main/java/com/example/studyjwt/jwt/JWTUtil.java
+++ src/main/java/com/example/studyjwt/jwt/JWTUtil.java
@@ -13,7 +13,7 @@ import io.jsonwebtoken.Jwts;
 @Component
 public class JWTUtil {
 
-    private SecretKey secretKey;
+    private final SecretKey secretKey;
 
     public JWTUtil(@Value("${spring.jwt.secret}")String secret) {
 
